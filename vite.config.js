5675920151790643468dded5cad3c6a43b79c289
Bagus Nuryasin <34181070+szczynk@users.noreply.github.com>
2024-03-12 12:46:14 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import million from 'million/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sky-clock/',
   plugins: [
     million.vite({ auto: true }),
     react(),
